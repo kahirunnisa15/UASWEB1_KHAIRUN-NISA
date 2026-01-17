014246cf55c1b7c397ec5b0a331231dedf43e98e
kahirunnisa15 <kahirunnisa25@gmail.com>
2026-01-17 20:37:17 +0700

    menambahkan koneksi

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_penjualann2"; // gunakan nama database yang benar
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
